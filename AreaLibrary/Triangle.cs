2574de95b5c0ad387703626f3b18907a932ff774
Darya <dashkanlol@gmail.com>
2024-09-05 20:43:19 +0300

    Добавила запрос

--- AreaLibrary/Triangle.cs
+++ AreaLibrary/Triangle.cs
@@ -22,7 +22,7 @@ namespace AreaLibrary
         public double CalculateArea()
         {
             if (a <= 0 || b <= 0 || c <= 0 || a + b <= c || a + c <= b || b + c <= a)
-                {
+            {
                 return 0;
             }
 
---
+++ SqlRequest.sql
@@ -0,0 +1,4 @@
+SELECT Products.ProductName, Categories.CategoryName
+FROM Products
+LEFT JOIN Categories 
+  ON Products.ProductID = Categories.CategoryID
